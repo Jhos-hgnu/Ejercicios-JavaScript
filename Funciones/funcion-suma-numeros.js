f3d080680d70f8c55a48da6ca9caf89a537ba1a0
Jhos-hgnu <hgjofer2004@gmail.com>
2025-09-04 23:01:15 -0600

    Funcion recursiva suma hasta n

---
+++ Funciones/funcion-suma-numeros.js
@@ -0,0 +1,14 @@
+/*Escribe una función recursiva llamada sumaHasta que reciba un número n y devuelva 
+la suma de todos los números desde 1 hasta n.
+
+Ejemplo esperado:
+
+sumaHasta(5) → 15
+
+sumaHasta(10) → 55 */
+
+function sumaHasta(numero){
+
+
+
+}
